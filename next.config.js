f8df103c7c22036b0cb4f2472ed70bc2a708a848
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-08-02 22:06:26 +0300

    adding pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const withPWA = require('next-pwa')
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['picsum.photos']
+  },
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true
   }
-}
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,31 @@ const Home: NextPage = () => {
       <div className={styles.container}>
           <Head>
               <title>Anthony Fink | Home</title>
+
+              <meta name='application-name' content='Anthony Fink | Full Stack Web Developer' />
+              <meta name='apple-mobile-web-app-capable' content='yes' />
+              <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+              <meta name='apple-mobile-web-app-title' content='Anthony Fink | Full Stack Web Developer' />
+              <meta name='description' content='Anthony Fink Resume | Full Stack Web Developer | Progressive Web Application with NextJs' />
+              <meta name='format-detection' content='telephone=no' />
+              <meta name='mobile-web-app-capable' content='yes' />
+              <meta name='msapplication-config' content='/icons/browserconfig.xml' />
+              <meta name='msapplication-TileColor' content='#2B5797' />
+              <meta name='msapplication-tap-highlight' content='no' />
+              <meta name='theme-color' content='#000000' />
+
+              <link rel='apple-touch-icon' href='/profile.png' />
+              <link rel='apple-touch-icon' sizes='152x152' href='/profile.png' />
+              <link rel='apple-touch-icon' sizes='180x180' href='/profile.png' />
+              <link rel='apple-touch-icon' sizes='167x167' href='/profile.png' />
+
+              <link rel='icon' type='image/png' sizes='32x32' href='/profile.png' />
+              <link rel='icon' type='image/png' sizes='16x16' href='/profile.png' />
+              <link rel='manifest' href='/manifest.json' />
+              <link rel='mask-icon' href='/profile.png' color='#5bbad5' />
+              <link rel='shortcut icon' href='/favicon.ico' />
+              <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
+
               <meta property="og:title" content="Anthony Fink | Full Stack Web Developer"></meta>
               <meta name="description" content="Anthony Fink | Full Stack Web Developer" />
               <link rel="icon" href="/profile.png" />

7b9ac10c722e743f536ad44f7f2a84dc7ad9c808
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2024-01-26 13:46:53 +0200

    fixing smart chat 502 gateway

--- pages/api/smartchat.ts
+++ pages/api/smartchat.ts
@@ -5,26 +5,28 @@ export default async function smartChat(req:Request,res:Response){
 	const openAi: OpenAIApi = new OpenAIApi({
 		apiKey: process.env.NEXT_PUBLIC_CHAT_GPT,
 	});
+	try {
+		const {question} = req.body;
 
-
-	const {question} = req.body;
-
-	if(!question) {
-		return res.status(422).json({"error":"Unprocessable Entity"})
+		if(!question) {
+			return res.status(422).json({"error":"Unprocessable Entity"})
+		}
+		const response: OpenAIApi.Chat.ChatCompletion = await openAi.chat.completions.create({
+			model: 'gpt-4-0125-preview',
+			messages:[
+				{
+					role: 'system',
+					content:'You are private assistant'
+				},
+				{
+					role:"user",
+					content:question
+				}
+			],
+		});
+		const pared = response.choices[0]?.message.content;
+		return res.status(200).json({answer:pared})
+	} catch (error) {
+		console.log(error)
 	}
-	const response: OpenAIApi.Chat.ChatCompletion = await openAi.chat.completions.create({
-		model: 'gpt-4-0125-preview',
-		messages:[
-			{
-				role: 'system',
-				content:'You are private assistant'
-			},
-			{
-				role:"user",
-				content:question
-			}
-		],
-	});
-	const pared = response.choices[0]?.message.content;
-	return res.status(200).json({answer:pared})
 }
